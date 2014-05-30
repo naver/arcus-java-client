38136ed135f7cb675e5dbd978badb59a0820b9fe
JunHyun Park <junhyun.park@navercorp.com>
2014-05-30 21:22:17 +0900

    fix unchecked warnings in MemcachedClient.java

--- src/main/java/net/spy/memcached/MemcachedClient.java
+++ src/main/java/net/spy/memcached/MemcachedClient.java
@@ -1085,7 +1085,7 @@ public class MemcachedClient extends SpyThread
 	 */
 	public <T> BulkFuture<Map<String, T>> asyncGetBulk(Collection<String> keys,
 		Transcoder<T> tc) {
-		return asyncGetBulk(keys, new SingleElementInfiniteIterator(tc));
+		return asyncGetBulk(keys, new SingleElementInfiniteIterator<Transcoder<T>>(tc));
 	}
 
 	/**
