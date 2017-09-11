a92ca3a3d7ca207b4ca41f3ad967e2296766677e
MinWooJin <alsdn653@jam2in.com>
2017-09-11 17:54:01 +0900

    FEATURE: Increase MAX_KEY_LENGTH: 250 => 32000

--- src/main/java/net/spy/memcached/MemcachedClientIF.java
+++ src/main/java/net/spy/memcached/MemcachedClientIF.java
@@ -18,7 +18,7 @@ public interface MemcachedClientIF {
 	/**
 	 * Maximum supported key length.
 	 */
-	int MAX_KEY_LENGTH = 250;
+	int MAX_KEY_LENGTH = 32000;
 
 	Collection<SocketAddress> getAvailableServers();
 
