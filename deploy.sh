f81734672a5eb56e16fb1e51f803f37d9ef24362
uhm0311 <uhm0311@jam2in.com>
2023-09-27 15:31:06 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
+mvn clean deploy $@
