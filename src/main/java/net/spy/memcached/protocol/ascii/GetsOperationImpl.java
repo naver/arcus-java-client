d535b7bce37ae1c87ff2d0e24c7762f908ea5dcc
cheesecrust <jack0810@kookmin.ac.kr>
2024-12-26 16:59:47 +0900

    INTERNAL: Integration get and mget api

--- src/main/java/net/spy/memcached/protocol/ascii/GetsOperationImpl.java
+++ src/main/java/net/spy/memcached/protocol/ascii/GetsOperationImpl.java
@@ -13,14 +13,15 @@ import net.spy.memcached.ops.GetsOperation;
 class GetsOperationImpl extends BaseGetOpImpl implements GetsOperation {
 
   private static final String CMD = "gets";
+  private static final String CMD_MGETS = "mgets";
 
   public GetsOperationImpl(String key, GetsOperation.Callback cb) {
     super(CMD, cb, Collections.singleton(key));
     setAPIType(APIType.GETS);
   }
 
-  public GetsOperationImpl(Collection<String> keys, GetsOperation.Callback cb) {
-    super(CMD, cb, new HashSet<>(keys));
+  public GetsOperationImpl(Collection<String> keys, GetsOperation.Callback cb, boolean isMGet) {
+    super(isMGet ? CMD_MGETS : CMD, cb, new HashSet<>(keys));
     setAPIType(APIType.GETS);
   }
 
