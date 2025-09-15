db7ee1ca5c534b25cde9e2a62757f69b18d5fdc8
uhm0311 <uhm0311@jam2in.com>
2025-09-15 17:45:54 +0900

    TEST: Temporarily disable touch test

--- src/test/manual/net/spy/memcached/collection/attribute/GetAttrTest.java
+++ src/test/manual/net/spy/memcached/collection/attribute/GetAttrTest.java
@@ -25,6 +25,7 @@ import net.spy.memcached.collection.CollectionResponse;
 import net.spy.memcached.collection.CollectionType;
 import net.spy.memcached.internal.CollectionFuture;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -108,6 +109,7 @@ class GetAttrTest extends BaseIntegrationTest {
   }
 
   @Test
+  @Disabled
   void testGetAttr_Touch() throws Exception {
     String key = "getattr_touch_attribute";
 
